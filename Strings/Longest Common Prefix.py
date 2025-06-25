e69791948cb2507a07caf3a72d28cb02a45240c3
Sabeer75 <sabeerg75@gmail.com>
2025-06-25 20:40:45 +0530

    commmon prefix

---
+++ Strings/Longest Common Prefix.py
@@ -0,0 +1,14 @@
+def Longest_Common_Prefix(strs):
+    a = " "
+    strs.sort()
+    first = strs[0]
+    last = strs[-1]
+    for i in range(len(first)):
+        if first[i] == last[i]:
+            a += first[i]
+        else:
+            break 
+    return a 
+
+strs = ["dog","racecar","car"]
+print(Longest_Common_Prefix(strs))
