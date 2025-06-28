71e1627c521f8f6b99ac07a4b55165e9aee6095d
Sabeer75 <sabeerg75@gmail.com>
2025-06-28 10:17:30 +0530

    longest

---
+++ Strings/Longest Repeating Character Replacement.py
@@ -0,0 +1,17 @@
+s = "ABABBA"
+k = 2
+
+count = {}
+max_len = 0 
+l = 0 
+
+for r in range(len(s)):
+    count[s[r]] = 1 + count.get(s[r],0)
+    frequent = max(count.values())
+
+    if (r - l + 1) - frequent > k:
+        count[s[l]] -= 1
+        l = 1 
+
+    max_len = max(r - l + 1 , max_len)
+print(max_len)
