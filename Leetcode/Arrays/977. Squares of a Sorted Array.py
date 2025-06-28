e6064b49a77f862c47b09184da6e8b06ce874f7d
Sabeer75 <sabeerg75@gmail.com>
2025-06-28 11:43:06 +0530

    leetcode folder

---
+++ Leetcode/Arrays/977. Squares of a Sorted Array.py
@@ -0,0 +1,22 @@
+nums = [-4,-1,0,3,10]
+res = []
+l = 0 
+r = len(nums) - 1 
+
+while l <= r:
+    if abs(nums[l]) > abs(nums[r]):
+        res.append(nums[l]**2)
+        l += 1 
+    else:
+        res.append(nums[r]**2)
+        r -= 1 
+print(res[::-1]) #timecomplexity = O(n)
+
+'''
+squared = []
+for i in nums:
+    squared.append(i*i)
+
+print(sorted(squared))
+# time complexity is O(n log n)
+'''
