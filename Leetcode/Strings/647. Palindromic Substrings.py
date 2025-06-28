47ac1ff2d47feb92283af72532887bab5bfa6115
Sabeer75 <sabeerg75@gmail.com>
2025-06-28 15:48:55 +0530

    palin

---
+++ Leetcode/Strings/647. Palindromic Substrings.py
@@ -0,0 +1,16 @@
+s = "abc"
+res_words = []
+
+for i in range(len(s)):
+    l,r = i,i
+    while l >= 0 and r < len(s) and s[l] == s[r]:
+        res_words.append(s[l:r+1])
+        l -= 1
+        r += 1 
+    
+    l,r = i, i+1
+    while l >= 0 and r < len(s) and s[l] == s[r]:
+        res_words.append(s[l:r+1])
+        l -= 1
+        r += 1
+print(len(res_words))
