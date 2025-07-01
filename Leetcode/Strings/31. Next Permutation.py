5a99ae5276a9955035e441c5ad88b2b99dcdfb8f
Sabeer75 <sabeerg75@gmail.com>
2025-07-01 23:06:23 +0530

    permu

---
+++ Leetcode/Strings/31. Next Permutation.py
@@ -0,0 +1,14 @@
+nums = [1,2,3]
+for i in range(len(nums) - 2, -1, -1):
+    if nums[i] < nums[i + 1]:
+        j = len(nums) - 1
+        while nums[j] <= nums[i]:
+            j -= 1
+        nums[j], nums[i] = nums[i], nums[j]
+        sortit = nums[i+1:]
+        sortit.sort()
+        nums[i+1:] = sortit
+        break
+else:
+    nums.sort()
+print(nums)
