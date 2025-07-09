f4f83b7a243f343d8b57597354889a16668a20ee
Sabeer75 <sabeerg75@gmail.com>
2025-07-09 23:54:11 +0530

    hi

---
+++ Code Forces/800/Game with Integers.py
@@ -0,0 +1,10 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    if n % 3 == 1:
+        print('First')
+    elif n%3 == 2:
+        print('First')
+    else:
+        if n%3 == 0:
+            print('Second')
