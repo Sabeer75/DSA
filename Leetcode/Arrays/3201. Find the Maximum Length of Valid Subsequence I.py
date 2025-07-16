63f73695e9042e1f141a6d3921aa5a49c7801366
Sabeer75 <sabeerg75@gmail.com>
2025-07-16 23:55:15 +0530

    isihf

---
+++ Leetcode/Arrays/3201. Find the Maximum Length of Valid Subsequence I.py
@@ -0,0 +1,10 @@
+nums = [1,2,3,4] 
+array = [[0,0],[1,1],[0,1],[1,0]] 
+res = 0  
+for pattern in array: 
+    count = 0  
+    for num in nums: 
+        if num % 2 == pattern[count%2]: 
+            count += 1 
+    res = max(res,count) 
+print(res)
