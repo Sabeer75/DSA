beb4f287ca07e557f9f7b8ad10f0d077437c11ef
Sabeer75 <sabeerg75@gmail.com>
2025-07-20 23:41:47 +0530

    klshdfg

--- Code Forces/800/Doremy's Paint 3.py
+++ Code Forces/800/Doremy's Paint 3.py
@@ -1,17 +1,22 @@
-t = int(input ())
+t = int(input())
 
 for _ in range(t): 
     n = int(input())
     a = list(map(int,input().split()))
 
-    if n == 2:
-        print("Yes")
-    elif n == 3:
-        if a[0] == a[1] or a[1] == a[2] or a[2] == a[0]:
+    mapl = {}
+    for i in a:
+        if i in mapl:
+            mapl[i] += 1 
+        else:
+            mapl[i] = 1
+    
+    if len(mapl) >= 3:
+        print("No")
+    else:
+        lis = sorted(mapl.values())
+        if len(lis) == 1 or lis[0] == lis[1] or (n % 2 == 1 and abs(lis[0] - lis[1])) == 1:
             print("Yes")
         else:
             print("No")
-    elif len(set(a)) == 1:
-        print("Yes")
-    else:
-        print("No")
+
