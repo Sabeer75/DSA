ec22acad55d63c3fac21fe865f7d99ecc9898f98
Sabeer75 <sabeerg75@gmail.com>
2025-07-26 11:30:13 +0530

    hi

---
+++ Code Forces/800/C. Target Practice.py
@@ -0,0 +1,8 @@
+t = int(input())
+for _ in range(t):
+    grid = [input().strip() for i in range(10)]
+    
+    for i in range(grid):
+        if "x" in i:
+            
+
--- Leetcode/Arrays/53. Maximum Subarray.py
+++ Leetcode/Arrays/53. Maximum Subarray.py
@@ -1,15 +1,15 @@
-#kadanes algorithm 
-nums = [5,4,-1,7,8]
-max_sum = float('-inf')
-curr_sum = 0 
+# kadanes algorithm
+nums = [-5, -4, -1, -7, -8]
+max_sum = float("-inf")
+curr_sum = 0
 for i in range(len(nums)):
     curr_sum += nums[i]
-    max_sum = max (curr_sum , max_sum )
+    max_sum = max(curr_sum, max_sum)
     if curr_sum < 0:
-        curr_sum = 0 
+        curr_sum = 0
 print(max_sum)
 
-''' 
+""" 
 #bruteforce 
 nums = [5,4,-1,7,8,9]
 
@@ -21,4 +21,4 @@ for i in range(n):
         curr_sum += nums[j]
         max_sum = max(max_sum,curr_sum)
 print(max_sum)
-'''   
+"""
