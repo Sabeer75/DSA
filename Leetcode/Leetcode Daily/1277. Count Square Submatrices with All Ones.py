f5fef3d5bdd6ee56c06a45e283e6715b0fa3adb9
Sabeer75 <sabeerg75@gmail.com>
2025-08-20 22:55:30 +0530

    hi

---
+++ Leetcode/Leetcode Daily/1277. Count Square Submatrices with All Ones.py
@@ -0,0 +1,10 @@
+matrix = [[0, 1, 1, 1], [1, 1, 1, 1], [0, 1, 1, 1]]
+row, col = len(matrix), len(matrix[0])
+dp = [[0] * (col + 1) for _ in range(row + 1)]
+ans = 0
+for i in range(row):
+    for j in range(col):
+        if matrix[i][j]:
+            dp[i + 1][j + 1] = min(dp[i][j + 1], dp[i + 1][j], dp[i][j]) + 1
+            ans += dp[i + 1][j + 1]
+print(ans)
