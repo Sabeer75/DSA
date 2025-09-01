061d36ec61286deb99ba3c9cad1a6b056c9d26eb
Sabeer75 <sabeerg75@gmail.com>
2025-09-01 23:09:31 +0530

    hi

--- test.py
+++ test.py
@@ -1,12 +1,6 @@
-height = [0,1,0,2,1,0,1,3,2,1,2,1]
-f= 0 
-rcount=0
-for i in range(len(height[f+1:])):
-    if height[f]<=height[i]:
-        rcount+=height[f]*(i-f+1)-sum(height[f:i])+f
-        f = i 
-    else:
-        i = max(height[f:])
-        rcount += height[i]*(i-f+1) - sum(height[f+1:i+1])+i
-        f = i 
-print(rcount)
+arr = [2, 7, 6, 1, 4, 5]
+for i in range(len(arr) - 1):
+    if i == 5:
+        print(arr[i])
+else:
+    print(0)
