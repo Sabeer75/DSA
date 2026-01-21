fdf28993bda01ef0dd89754e7ebd238c1d124ec4
Sabeer75 <sabeerg75@gmail.com>
2026-01-21 19:59:42 +0530

    quicksort

--- Leetcode/Algorithms/merge_sort.py
+++ Leetcode/Algorithms/merge_sort.py
@@ -31,3 +31,34 @@ def merge_sort(arr):
 arr_test = [2, 3, 5, 1, 7, 4, 4, 4, 2, 6, 0]
 merge_sort(arr_test)
 print(arr_test)
+"""
+another way 
+def merge_sort(arr, l, r):
+    if l >= r:
+        return
+
+    mid = (l + r) // 2
+    merge_sort(arr, l, mid)
+    merge_sort(arr, mid + 1, r)
+
+    temp = []
+    i, j = l, mid + 1
+    while i <= mid and j <= r:
+        if arr[i] <= arr[j]:
+            temp.append(arr[i])
+            i += 1
+        else:
+            temp.append(arr[j])
+            j += 1
+    while i <= mid:
+        temp.append(arr[i])
+        i += 1
+
+    while j <= r:
+        temp.append(arr[j])
+        j += 1
+
+    for k in range(len(temp)):
+        arr[l + k] = temp[k]
+    return arr
+"""
